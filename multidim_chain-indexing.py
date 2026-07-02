e8eeb0ce6ba77865ab39c300cf8525f5873c657f
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-07-03 02:05:39 +0530

    chain-index

---
+++ multidim_chain-indexing.py
@@ -0,0 +1,7 @@
+import numpy as np
+arr=np.array([[["A","B","C"],["D","E","F"],["G","H","I"]],
+                [["J","K","L"],["M","N","O"],["P","Q","R"]],
+                [["S","T","U"],["V","W","X"],["Y","Z",".."]]])
+print(arr[0][1][1],end="")
+print(arr[0][2][0],end="")
+print(arr[0][2][0])
