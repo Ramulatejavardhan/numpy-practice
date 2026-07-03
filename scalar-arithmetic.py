0f9e8586843f249065e83d79fa31687800ed9521
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-07-03 15:59:58 +0530

    arithmetic

--- scalar-arithmetic.py
+++ scalar-arithmetic.py
@@ -1,5 +1,7 @@
 import numpy as np
-arr=np.array([1,2,3])
-print(arr+1)
-print(arr*2)
-print(arr**2)#power of 2 "arr^2"
+arr1=np.array([1,2,3])
+print(arr1+1)
+print(arr1*2)
+print(arr1**2)#power of 2 "arr^2"
+arr2=np.array([4,5,6])
+print("sum of 2 arrays:",arr1+arr2)
---
+++ vectorizedmathfunc.py
@@ -0,0 +1,5 @@
+import numpy as np
+arr=np.array([1,2,3,4])
+print(np.sqrt(arr))
+arr1=np.array([1.232,2.343,3.95])
+print(np.round(arr1))
