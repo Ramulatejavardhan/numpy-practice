56037e649aca4eede1754907a59309e1469d91db
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-07-03 22:27:21 +0530

    aggregatefunc

---
+++ aggregatefunc.py
@@ -0,0 +1,8 @@
+import numpy as np
+arr=np.array([1,2,3])
+#print(np.mean(arr))#mean value
+#print(np.sum(arr))#addtion
+#print(np.std(arr))#standard deviation
+#print(np.var(arr))#variance
+print(np.argmin(arr))#index value or position of min value.
+print(np.argmax(arr))#index value or position of max value.
